b76e0fe1335475fc44dfe1957af7c305ed5be8fe
Massimo Manco <massimomanco80@gmail.com>
2022-05-07 16:32:29 +0200

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FE0222A';
+  todos : Todo[] = [];
+  newTodo : string;
+
+  saveTodo(){
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.title = this.newTodo;
+      todo.completed = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    }else{
+      alert('inserisci un task');
+    }
+  }
+  completato(id:number){
+    this.todos[id].completed = !this.todos[id].completed;
+
+  }
 }
